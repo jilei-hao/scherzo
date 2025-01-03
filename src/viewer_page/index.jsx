1c62fd50dc2e6690178a0e8ee0e499926f577b1a
jilei-hao <jilei.cs@gmail.com>
2025-01-02 23:30:16 -0500

    model rendered

--- src/viewer_page/index.jsx
+++ src/viewer_page/index.jsx
@@ -9,10 +9,14 @@ import vtkRenderWindow from '@kitware/vtk.js/Rendering/Core/RenderWindow';
 import vtkRenderWindowInteractor from '@kitware/vtk.js/Rendering/Core/RenderWindowInteractor';
 import vtkRenderer from '@kitware/vtk.js/Rendering/Core/Renderer';
 import vtkInteractorStyleTrackballCamera from '@kitware/vtk.js/Interaction/Style/InteractorStyleTrackballCamera';
+import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor';
+import vtkMapper from "@kitware/vtk.js/Rendering/Core/Mapper";
 
 export default function ViewerPage(props) {
   const rwContainerRef = useRef(null);
   const rwRef = useRef(null);
+
+  console.log("[ViewerPage]: models", props.models);
 
   useEffect(() => {
     if (rwRef.current)
@@ -22,14 +26,33 @@ export default function ViewerPage(props) {
     const rwView = rw.newAPISpecificView();
     rw.addView(rwView);
 
-    console.log("[ViewPage] rwContainerRef", rwContainerRef.current);
+    // configure renderer
+    const ren = vtkRenderer.newInstance({ background: [0.9, 0.9, 0.9] });
+    rw.addRenderer(ren);
+
+    // configure interactor
+    const interactor = vtkRenderWindowInteractor.newInstance();
+    interactor.setView(rwView);
+    interactor.initialize();
+    interactor.setInteractorStyle(vtkInteractorStyleTrackballCamera.newInstance());
+
+
+
+    const actor = vtkActor.newInstance();
+    const mapper = vtkMapper.newInstance();
+    mapper.setInputData(props.models[2]);
+    actor.setMapper(mapper);
+    ren.addActor(actor);
+    ren.resetCamera();
+
+
     rwView.setContainer(rwContainerRef.current);
     rwRef.current = rw;
+    rw.render();
   }, []);
 
   return (
-    <div>
-      <h1>Viewer</h1>
+    <div className={styles.pageContainer}>
       <div className={styles.renderWindowContainer} ref={rwContainerRef}/>
     </div>
   );
