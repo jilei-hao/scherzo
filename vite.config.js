d3f89e2c11dcd8989390b73b832102a96d361be5
jilei-hao <jilei.cs@gmail.com>
2025-05-28 14:41:34 -0400

    troubleshooting deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/scherzo/', // for GitHub Pages deployment
+  base: '/', // for GitHub Pages deployment
   plugins: [react()],
   server: {
     port: 3001,
