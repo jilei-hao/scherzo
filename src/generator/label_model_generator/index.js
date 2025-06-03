d22cab8d2b4e92d266a2236f12f5aba37a4b71bb
jilei-hao <jilei.cs@gmail.com>
2025-06-03 08:03:46 -0500

    added AGPL-3.0 license

--- src/generator/label_model_generator/index.js
+++ src/generator/label_model_generator/index.js
@@ -1,3 +1,20 @@
+// This file is part of the Scherzo project.
+// Copyright (C) 2025 Jilei Hao
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 import { convertItkToVtkImage } from '@kitware/vtk.js/Common/DataModel/ITKHelper'
 import vtkImageMarchingCubes from '@kitware/vtk.js/Filters/General/ImageMarchingCubes';
 import vtkWindowedSincPolyDataFilter from '@kitware/vtk.js/Filters/General/WindowedSincPolyDataFilter';
--- src/viewer_page/icon_buttons/index.jsx
+++ src/viewer_page/icon_buttons/index.jsx
@@ -1,3 +1,20 @@
+// This file is part of the Scherzo project.
+// Copyright (C) 2025 Jilei Hao
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 import IconStepForward_Idle from '@assets/icons/step-forward_idle.svg';
 import IconStepBack_Idle from '@assets/icons/step-back_idle.svg';
 import IconPlay_Idle from '@assets/icons/play_idle.svg';
